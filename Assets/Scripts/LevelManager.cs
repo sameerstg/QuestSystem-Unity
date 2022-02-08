0aaa6fb7e6f873b3aa9ccf4b6398a4cb40b57e69
Sameer Ahmed <sameerstg@outlook.com>
2022-02-09 00:21:48 +0500

    First Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelManager
+{
+        public void LoadNextLevel()
+        {
+
+        }
+}
+public interface IQuestManager
+{
+    int TotalQuest { get; set;}
+    int CompletedQuest { get; set;}
+    
+    void GenerateQuest(int TotalQuest);
+    void QuestCompleted();
+    void QuestInstantiation();
+    void QuestCondition();
+    void EvaluteQuest();
+    void AllQuestCompleted();
+}
+public class QuestSystem
+{
+    public void QuestCondition()
+    {
+
+    }
+    
+}
+public interface GoalManager
+{   
+    /*
+    Amount Of Required Goal
+    Amount Of Completed Goal
+    Method to define description and logic of Goal
+    Method To Evaluate if Goal is completed
+    Method to Evaluate Amount of Completed Goal
+    Method to Tell Quest Manager about Goal Completion*/
+}
+
---
+++ Assets/Scripts/LevelOne.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOne : MonoBehaviour, IQuestManager
+{
+    //      Setting For New Level
+    [Header("Setting For Level")]
+    //      Total Number Of Level
+    public int totalQuest = 3;
+    //      Completed Level
+    int completedQuests;
+    //      List of Quest Names
+    public string[] questNames;
+    //      List of Quest Description
+    [TextArea]
+    public string[] questDescription;
+    //      Array of Bool for Quest Array
+    public bool[] isQuestCompleted;
+
+
+
+    //      Interface Properties Getter and Setter
+    public int TotalQuest { get => totalQuest; set => totalQuest = value; }
+    public int CompletedQuest { get => completedQuests; set => completedQuests = value; }
+    //      Method To be called from inspector
+    [ContextMenu("QuestInstantiation")]
+    public void QuestInstantiation()
+    {
+        GenerateQuest(TotalQuest);
+    }
+    //      Instantiating Quests Array
+    public void GenerateQuest(int TotalQuest)
+    {
+         questNames =  new string[TotalQuest];
+         questDescription =  new string[TotalQuest];
+        isQuestCompleted = new bool[TotalQuest];
+        QuestCondition();
+
+    }
+    
+    //      Evaluating If All Quest are done
+    public void EvaluteQuest()
+    {
+        if (CompletedQuest >= TotalQuest)
+        {
+            AllQuestCompleted();
+        }
+    }
+    //      Called After Each Quest Completion
+    public void QuestCompleted()
+    {
+        CompletedQuest += 1;
+        EvaluteQuest();
+    }
+    //      Called after completion of All Quest
+    public void AllQuestCompleted()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void QuestCondition()
+    {
+        if (!isQuestCompleted[0])
+        {
+            if (false)
+            {
+                QuestCompleted();
+                isQuestCompleted[0] = true;
+            }
+        }
+    }
+}
